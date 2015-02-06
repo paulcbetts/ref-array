b1ff9eb8c3ac505f8b20e3187370e2d03a94f650
Paul Betts <paul@paulbetts.org>
2015-02-06 14:19:01 -0800

    Fix tests

--- test/array.js
+++ test/array.js
@@ -1,6 +1,6 @@
 
 var assert = require('assert')
-  , ref = require('ref')
+  , ref = require('ref-atomshell-paulcbetts')
   , ArrayType = require('../')
   , bindings = require('bindings')({ module_root: __dirname, bindings: 'native_tests' })
 
